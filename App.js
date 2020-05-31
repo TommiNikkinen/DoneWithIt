0fd1fba3db4233ced0babebf4401adab3f468ccb
Tommi Nikkinen <Tommi.Nikkinen2@edu.savonia.fi>
2020-05-31 14:39:50 +0300

    jsoncofing and launch file

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("app executed");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -12,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
