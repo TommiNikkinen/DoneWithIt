134d9ac952f6a4dde2cecb51861371e410021aa2
Tommi Nikkinen <Tommi.Nikkinen2@edu.savonia.fi>
2020-07-01 00:08:03 +0300

    Picker and textinput added

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
-import { Text, TextInput } from "react-native";
+import { Text, TextInput, Switch } from "react-native";
 
 import Screen from "./app/components/Screen/Screen";
+import AppText from "./app/components/AppText/AppText";
+import AppTextInput from "./app/components/AppTextInput/AppTextInput";
+import AppPicker from "./app/components/AppPicker/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [category, setCategory] = useState(categories[0]);
   return (
     <Screen>
-      <Text>{firstName}</Text>
-      <TextInput
-        placeholder="firstname"
-        onChangeText={(e) => setFirstName(e)}
-        value={firstName}
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
       />
+      <AppTextInput icon="email" placeholder="email" />
     </Screen>
   );
 }
